bba62210b2bd40f2a62b6a9dbe4189e2963df5ad
kartik sharma <kartiksharma88103@gmail.com>
2024-04-05 12:17:23 +0530

    database part using php is completed

--- first.php
+++ first.php
@@ -3,13 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Successfull Submitted</title>
 </head>
 <body>
-    <div class="f">
-        <?php
-        echo "your form is submitted" 
-        ?>
-    </div>
-</body>
+    <?php
+    // connecting to local server
+    $servername ="localhost";
+    $password = "";
+    $username = "root";
+    $con = mysqli_connect($servername ,$username ,$password);
+
+    if(!$con){
+        die("connection failed :" . mysqli_connect_error());
+    }
+    // echo "successful connected";
+
+    // fetching details from the html page 
+    $name = $_POST['firstName'];
+    $phone= $_POST['contact'];
+    $email= $_POST['email'];
+    $address= $_POST['Address'];
+    $feedback= $_POST['feedBack'];
+
+
+    $sql = "INSERT INTO `form_details`.`details` (`Time`, `name`, `number`, `email`, `address`, `feedback`) VALUES (current_timestamp(),'$name','$phone','$email','$address','$feedback')";
+    
+    if($con->query($sql) == true){
+        echo "data is succesfully inserted";
+    }
+    else{
+        echo "data is not inserted error =",$con->error;
+    }
+    ?>
+    </body>
 </html>
